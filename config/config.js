c051fbaf04ba83136b7da41ec00d14935a42b720
evariste <musekwa2011@gmail.com>
2022-06-23 22:27:05 +0200

    deploying

--- config/config.js
+++ config/config.js
@@ -2,11 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  env: process.env.NODE_ENV || "development",
-  port: process.env.PORT || 5000,
-  jwtSecret: process.env.JWT_SECRET || "meitw2022@estudante",
-  mongoURI: process.env.MONGODB_URI ? process.env.MONGODB_URI :
-    "mongodb+srv://musekwa:meitw2022estudante@siscaju.8uwsr.mongodb.net/productionDB?retryWrites=true&w=majority",
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
+  mongoURI: process.env.MONGODB_URI
 };
 
 export default config;
