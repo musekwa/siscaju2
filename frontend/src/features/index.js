dd3a577aecd7a01930fb0640c3be315e7adb0e7f
evariste <musekwa2011@gmail.com>
2022-06-24 08:10:56 +0200

    done

--- frontend/src/features/index.js
+++ frontend/src/features/index.js
@@ -1,3 +1,3 @@
 
-export const baseURL = "https://siscaju.herokuapp.com/";
+export const baseURL = "https://siscaju.herokuapp.com";
 // export const baseURL = 'https://infinite-escarpment-55646.herokuapp.com'
